280c2932b6bb31b2550a0e34f6b6153bb10229cc
patrickpahl <ppahl84@yahoo.com>
2016-07-06 11:21:09 -0600

    Worked on ThreadController

--- SocialSuburb/ThreadDetailTableViewController.swift
+++ SocialSuburb/ThreadDetailTableViewController.swift
@@ -9,87 +9,116 @@
 import UIKit
 
 class ThreadDetailTableViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var textField: UITextField!
+    
+    var thread: Thread?
+    
+    // MARK: IBActions
+    @IBAction func sendButtonTapped(sender: AnyObject) {                                ///Send Code ADDED
+        
+        guard let messageText = textField.text else {return}
+        textField.resignFirstResponder()
+        
+        // TESTING
+        if let thread = thread {
+            ThreadController.sharedController.addMessageToThread(messageText, thread: thread) { (success) in
+                /// CODE HERE???
+            }
+        }
     }
-
+    
+    func messagesWereUpdated(notification: NSNotification) {                             /// Messages were updated ADDED
+        tableView.reloadData()
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {                        ///Text field should return ADDED
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func viewDidLoad() {                                                       ///VDL: Need NSNotificationCenter or addObserver code???
+        super.viewDidLoad()
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+    ///Num of Sections in Table View: Commented out
+    //    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    //        // #warning Incomplete implementation, return the number of sections
+    //        return 0
+    //    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {        ///Num rows in section: Need to complete
+        //            ThreadController.sharedController.
         return 0
     }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
+    
+    let dateFormatter: NSDateFormatter = {                                              ///Date formatter: Need this?
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .ShortStyle
+        formatter.doesRelativeDateFormatting = true
+        formatter.timeStyle = .ShortStyle
+        return formatter
+    }()
+    
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCellWithIdentifier("MessageCell", forIndexPath: indexPath)
+        
+        
         return cell
     }
-    */
-
+    
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+     if editingStyle == .Delete {
+     // Delete the row from the data source
+     tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+     } else if editingStyle == .Insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- ThreadController.swift
+++ ThreadController.swift
@@ -10,7 +10,6 @@ import Foundation
 import CoreData
 import UIKit
 
-
 class ThreadController{
     
     static let sharedController = ThreadController()
@@ -21,9 +20,9 @@ class ThreadController{
     
     init(){
         
-    self.cloudKitManager = CloudKitManager()
-    
-    ///performFullSync()
+        self.cloudKitManager = CloudKitManager()
+        
+        performFullSync()
         
     }
     
@@ -39,9 +38,11 @@ class ThreadController{
     
     
     func addThread(name: String, text: String, completion: (() -> Void)?){
-        let thread = Thread(name: name)
+        guard let thread = Thread(name: name) else {
+            return
+        }
         
-        addMessageToThread(text, thread: name, completion: nil)                 ///???
+        addMessageToThread(text, thread: thread, completion: nil)
         
         saveToPersistentStorage()
         
@@ -49,14 +50,14 @@ class ThreadController{
             completion()
         }
         
-        if let threadRecord = thread?.cloudKitRecord {
+        if let threadRecord = thread.cloudKitRecord {
             
             cloudKitManager.saveRecord(threadRecord, completion: { (record, error) in
                 
                 if let record = record {
-                    thread?.update(record)
+                    thread.update(record)
                     
-                ///subscription code??
+                    ///subscription code??
                 }
             })
         }
@@ -70,27 +71,21 @@ class ThreadController{
         if let completion = completion{
             completion()
         }
-        ////Delete CloudKit Code:
+        ///Delete CloudKit Code:
         
-        if let record = thread.cloudKitRecord{
+        if let CKRecordID = thread.cloudKitRecordID{
             
-            cloudKitManager.deleteRecordWithID(CKRecordID(recordName: recordID), completion: { (recordID, error) in
+            cloudKitManager.deleteRecordWithID(CKRecordID, completion: { (recordID, error) in
                 NSLog("OK or \(error)")
             })
         }
-        
-//        FROM STACKOVERFLOW
-//        database.deleteRecordWithID(CKRecordID(recordName: recordId), completionHandler: {recordID, error in
-//            NSLog("OK or \(error)")
-//        }
-        
     }
     
     func addMessageToThread(text: String, thread: Thread, completion: ((success: Bool) -> Void)?){
         
         let message = Message(thread: thread, text: text)
         
-        saveToPersistentStorage()
+        saveToPersistentStorage()                               ///Takes care of 'self.messages.append(message)' ???
         
         if let completion = completion{
             completion(success: true)
@@ -115,12 +110,13 @@ class ThreadController{
             completion()
         }
         
-        ////////Delete in CloudKit code
+        ///Delete in CloudKit code
         
-        if let messageRecord = messsage?.cloudkitRecord {
-        
-        cloudKitManager.saveRecord(messageRecord) { (record, error) in
-            }
+        if let messageRecordID = message.cloudKitRecordID {
+            
+            cloudKitManager.deleteRecordWithID(messageRecordID, completion: { (recordID, error) in
+                NSLog("OK or \(error)")
+            })
         }
     }
     
@@ -155,15 +151,15 @@ class ThreadController{
     }
     
     func unsyncedRecords(type: String) -> [CloudKitManagedObject] {
-    
-    let fetchRequest = NSFetchRequest(entityName: type)
-    let predicate = NSPredicate(format: "recordData == nil")
-    
-    fetchRequest.predicate = predicate
         
-    let results = (try? Stack.sharedStack.managedObjectContext.executeFetchRequest(fetchRequest)) as? [CloudKitManagedObject] ?? []
+        let fetchRequest = NSFetchRequest(entityName: type)
+        let predicate = NSPredicate(format: "recordData == nil")
         
-    return results
+        fetchRequest.predicate = predicate
+        
+        let results = (try? Stack.sharedStack.managedObjectContext.executeFetchRequest(fetchRequest)) as? [CloudKitManagedObject] ?? []
+        
+        return results
     }
     
     // MARK: - Sync
@@ -179,7 +175,7 @@ class ThreadController{
             
             pushChangesToCloudKit({ (success, error) in
                 self.fetchNewRecords(Thread.typeKey){
-                    self.fetchNewRecords(Message.typeKey, completion: { 
+                    self.fetchNewRecords(Message.typeKey, completion: {
                         self.isSyncing = false
                         
                         if let completion = completion{
@@ -213,35 +209,30 @@ class ThreadController{
             }
             self.saveToPersistentStorage()
             
-            }) { (records, error) in
-                
-                if error != nil{
-                    print("error - fetch new records")
-                }
-                if let completion = completion{
-                    completion()
-                }
+        }) { (records, error) in
+            
+            if error != nil{
+                print("error - fetch new records")
+            }
+            if let completion = completion{
+                completion()
             }
         }
-    
-    
-    
-    
-    
+    }
     
     func pushChangesToCloudKit(completion: ((success: Bool, error: NSError?) -> Void)?) {
         
-     let unsavedManagedObjects = unsyncedRecords(Thread.typeKey) + unsyncedRecords(Message.typeKey)
-     let unsavedRecords = unsavedManagedObjects.flatMap({ $0.cloudKitRecord})
+        let unsavedManagedObjects = unsyncedRecords(Thread.typeKey) + unsyncedRecords(Message.typeKey)
+        let unsavedRecords = unsavedManagedObjects.flatMap({ $0.cloudKitRecord})
         
-     cloudKitManager.saveRecords(unsavedRecords, perRecordCompletion: { (record, error) in
-        
-        guard let record = record else {return}
-        
-        if let matchingRecord = unsavedManagedObjects.filter({$0.recordName == record.recordID.recordName}).first{
-            matchingRecord.update(record)
-        }
-        
+        cloudKitManager.saveRecords(unsavedRecords, perRecordCompletion: { (record, error) in
+            
+            guard let record = record else {return}
+            
+            if let matchingRecord = unsavedManagedObjects.filter({$0.recordName == record.recordID.recordName}).first{
+                matchingRecord.update(record)
+            }
+            
         }) { (records, error) in
             if let completion = completion{
                 let success = records != nil
@@ -249,5 +240,138 @@ class ThreadController{
             }
         }
     }
+    
+    // MARK: - Subscriptions
+    
+    func subscribeToNewThreads(completion: ((success: Bool, error: NSError?) -> Void)?){
+        
+        let predicate = NSPredicate(value: true)
+        
+        cloudKitManager.subscribe(Thread.typeKey, predicate: predicate, subscriptionID: "allThreads", contentAvailable: true, options: .FiresOnRecordCreation) { (subscription, error) in
+            
+            if let completion = completion{
+                let success = subscription != nil
+                completion(success: success, error: error)
+            }
+        }
+    }
+    
+    func checkSubscriptionToThreadMessages(thread: Thread, completion: ((subscribed: Bool) -> Void)?) {
+        
+        cloudKitManager.fetchSubscription(thread.recordName) { (subscription, error) in
+            if let completion = completion {
+                let subscribed = subscription != nil
+                completion(subscribed: subscribed)
+            }
+        }
+    }
+    
+    func addSubscriptionToThreadMessages(thread: Thread, alertBody: String?, completion: ((success: Bool, error: NSError?) -> Void)?){
+        
+        guard let recordID = thread.cloudKitRecordID else {fatalError("Unable to create cloudkit reference for subscription")}
+        
+        let predicate = NSPredicate(format: "thread == %@", argumentArray: [recordID])
+        
+        cloudKitManager.subscribe(Message.typeKey, predicate: predicate, subscriptionID: thread.recordName, contentAvailable: true, options: .FiresOnRecordCreation) { (subscription, error) in
+            
+            if let completion = completion{
+                
+                let success = subscription != nil
+                completion(success: success, error: error)
+            }
+        }
+    }
+    
+    func removeSubscriptionToThreadMessages(thread: Thread, completion: ((success: Bool, error: NSError?) -> Void)?) {
+        
+        let subscriptionID = thread.recordName
+        
+        cloudKitManager.unsubscribe(subscriptionID) { (subscriptionID, error) in
+            if let completion = completion {
+                let success = subscriptionID != nil && error == nil
+                completion(success: success, error: error)
+            }
+        }
+    }
+    
+    func toggleThreadMessageSubscription(thread: Thread, completion: ((success: Bool, isSubscribed: Bool, error: NSError?) -> Void)?) {
+        
+        cloudKitManager.fetchSubscriptions { (subscriptions, error) in
+            if subscriptions?.filter({$0.subscriptionID == thread.recordName}).first != nil {
+                self.removeSubscriptionToThreadMessages(thread, completion: { (success, error) in
+                    
+                    if let completion = completion{
+                        completion(success: success, isSubscribed: false, error: error)
+                    }
+                })
+            } else {
+                self.addSubscriptionToThreadMessages(thread, alertBody: "new Message 👍", completion: { (success, error) in
+                    if let completion = completion{
+                        completion(success: true, isSubscribed: true, error: error)
+                    }
+                })
+            }
+        }
+    }
+    
+    /////////////////////////////////////////////////////// NEW 7/3/16, from Message Controller on bulletinboard app
+    
+    static let MessagesControllerDidRefreshNotification = "MessagesControllerDidRefreshNotification"
+    
+    static let ThreadsControllerDidRefreshNotification = "ThreadsControllerDidRefreshNotification"
+    
+    
+    // MARK: Public Methods
+    
+    func refresh(completion: ((NSError?) -> Void)? = nil){
+        
+        cloudKitManager.fetchRecordsWithType(Message.recordType, recordFetchedBlock: nil) { (records, error) in
+            defer {completion?(error)}
+            if let error = error {
+                NSLog("Error fetching from CloudKit: \(error)")
+                return
+            }
+            guard let records = records else {return}
+            self.messages = records.flatMap{Message(cloudKitRecord: $0)}.sort {$0.date.compare($1.date) == NSComparisonResult.OrderedAscending}
+        }
+    }
+    
+    // MARK: Public Properties
+    
+    static var messages = [Message](){
+        
+        didSet {
+            dispatch_async(dispatch_get_main_queue()) {
+                let nc = NSNotificationCenter.defaultCenter()
+                nc.postNotificationName(ThreadController.MessagesControllerDidRefreshNotification, object: self)
+            }
+        }
+    }
+    
+    static var threads = [Thread](){
+        didSet {
+            dispatch_async(dispatch_get_main_queue()) {
+                let nc = NSNotificationCenter.defaultCenter()
+                nc.postNotificationName(ThreadController.ThreadsControllerDidRefreshNotification, object: self)
+            }
+        }
+        
+        
+        
+        
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
